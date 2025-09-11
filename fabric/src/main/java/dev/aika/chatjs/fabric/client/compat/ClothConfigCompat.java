a476e8025d7ca446fdab7c9bc02e085f6abfc83f
Gizmo <me@aika.dev>
2025-09-11 08:42:39 +0800

    fix: incorrect default value in settings screen

--- fabric/src/main/java/dev/aika/chatjs/fabric/client/compat/ClothConfigCompat.java
+++ fabric/src/main/java/dev/aika/chatjs/fabric/client/compat/ClothConfigCompat.java
@@ -3,6 +3,7 @@ package dev.aika.chatjs.fabric.client.compat;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import dev.aika.chatjs.client.gui.MissingClothConfigScreen;
+import dev.aika.chatjs.client.gui.ClothConfigScreen;
 import dev.architectury.platform.Platform;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -12,8 +13,7 @@ public class ClothConfigCompat implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return parent -> {
-            if (Platform.isModLoaded("cloth-config2"))
-                return dev.aika.chatjs.compat.ClothConfigCompat.ConfigScreen(parent);
+            if (Platform.isModLoaded("cloth-config2")) return ClothConfigScreen.create(parent);
             return new MissingClothConfigScreen(parent);
         };
     }
--- forge/src/main/java/dev/aika/chatjs/forge/compat/ClothConfigCompat.java
+++ forge/src/main/java/dev/aika/chatjs/forge/compat/ClothConfigCompat.java
@@ -1,14 +1,14 @@
 package dev.aika.chatjs.forge.compat;
 
 import dev.aika.chatjs.client.gui.MissingClothConfigScreen;
+import dev.aika.chatjs.client.gui.ClothConfigScreen;
 import dev.architectury.platform.Platform;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 
 public class ClothConfigCompat {
     public static Screen setup(Minecraft ignoredMinecraft, Screen parent) {
-        if (Platform.isModLoaded("cloth_config"))
-            return dev.aika.chatjs.compat.ClothConfigCompat.ConfigScreen(parent);
+        if (Platform.isModLoaded("cloth_config")) return ClothConfigScreen.create(parent);
         else return new MissingClothConfigScreen(parent);
     }
 }
