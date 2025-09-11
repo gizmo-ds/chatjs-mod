7a9c37a44b5abe746e30a76e82167ab75fe88114
Gizmo <me@aika.dev>
2025-09-12 02:21:02 +0800

    fix: invalid function call

--- example/server_scripts/function_calling/AtAI.js
+++ example/server_scripts/function_calling/AtAI.js
@@ -7,7 +7,7 @@ const tools = [
             parameters: {
                 type: "object",
                 properties: {
-                    mode: {type: "command", description: "需要执行的minecraft指令"},
+                    command: { type: "string", description: "需要执行的minecraft指令" },
                 },
                 required: ["command"],
             },
@@ -16,7 +16,7 @@ const tools = [
 ];
 
 function send_messages(msgs) {
-    let result = openai.chat.createCompletion(JSON.stringify({messages: msgs, tools: tools, tool_choice: "auto"}));
+    let result = openai.chat.createCompletion(JSON.stringify({ messages: msgs, tools: tools, tool_choice: "auto" }));
     result = JSON.parse(JsonIO.toString(result));
     return result.choices[0].message;
 }
@@ -24,7 +24,7 @@ function send_messages(msgs) {
 PlayerEvents.chat((e) => {
     if (!e.message.toLowerCase().startsWith("@ai ")) return;
     const msg = e.message.substring(4);
-    const messages = [{role: "user", content: `${e.getUsername()} Say: ${msg}`}];
+    const messages = [{ role: "user", content: `${e.getUsername()} Say: ${msg}` }];
 
     let message = send_messages(messages);
     const tool_calls = message.tool_calls;
@@ -34,23 +34,25 @@ PlayerEvents.chat((e) => {
         e.server.scheduleInTicks(1, (_) => e.server.tell(Text.of("Failed!")));
     }
 
-    if (tool_calls && tool_calls.length) {
-        var tool = tool_calls[0];
-        switch (tool.function.name) {
-            case "exec_command": {
-                messages.push(message);
-                messages.push({role: "tool", tool_call_id: tool.id, content: "success"});
-                var args = JSON.parse(tool.function.arguments);
-                e.server.runCommand(args.command);
-                console.log(`AI exec: ${args.command}`);
-                break;
+    if (tool_calls && tool_calls.length > 0) {
+        messages.push(message);
+        for (const tool of tool_calls) {
+            switch (tool.function.name) {
+                case "exec_command": {
+                    messages.push({ role: "tool", tool_call_id: tool.id, content: "success" });
+                    var args = JSON.parse(tool.function.arguments);
+                    e.server.runCommand(args.command);
+                    console.log(`AI exec: ${args.command}`);
+                    break;
+                }
+                default:
+                    console.warn(`Unknown function: ${tool.function.name} ${tool.function.arguments}`);
+                    return e.cancel();
             }
-            default:
-                console.warn(`Unknown function: ${tool.function.name} ${tool.function.arguments}`);
-                return e.cancel();
         }
         message = send_messages(messages);
-        if (message.content !== "")
+        if (message.content !== "") {
             e.server.scheduleInTicks(1, (_) => e.server.tell(Text.of("§bAI Say: §f" + message.content)));
+        }
     }
 });
